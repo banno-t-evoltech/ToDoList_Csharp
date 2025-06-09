3a5dce863c006dbbe79ad4d865e7f05b4e28fc71
sinneon <sinneon.world@gmail.com>
2025-06-09 14:08:22 +0900

    プロジェクト ファイルを追加します。

---
+++ DataAccess.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+
+public class DataAccess
+{
+    private string connectionString = "Data Source=todolist.db;Version=3;";
+
+    public DataAccess()
+    {
+        InitializeDatabase();
+    }
+
+    private void InitializeDatabase()
+    {
+        if (!File.Exists("todolist.db"))
+        {
+            SQLiteConnection.CreateFile("todolist.db");
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string createTableQuery = @"
+                    CREATE TABLE IF NOT EXISTS Todos (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Task TEXT NOT NULL,
+                        IsCompleted INTEGER NOT NULL
+                    );";
+                using (var command = new SQLiteCommand(createTableQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+
+    public List<TodoItem> GetTodos()
+    {
+        List<TodoItem> todos = new List<TodoItem>();
+        using (var connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            string selectQuery = "SELECT Id, Task, IsCompleted FROM Todos;";
+            using (var command = new SQLiteCommand(selectQuery, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        todos.Add(new TodoItem
+                        {
+                            Id = reader.GetInt32(0),
+                            Task = reader.GetString(1),
+                            IsCompleted = reader.GetInt32(2) == 1
+                        });
+                    }
+                }
+            }
+        }
+        return todos;
+    }
+
+    public void AddTodo(TodoItem todo)
+    {
+        using (var connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            string insertQuery = "INSERT INTO Todos (Task, IsCompleted) VALUES (@Task, @IsCompleted);";
+            using (var command = new SQLiteCommand(insertQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Task", todo.Task);
+                command.Parameters.AddWithValue("@IsCompleted", todo.IsCompleted ? 1 : 0);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void UpdateTodo(TodoItem todo)
+    {
+        using (var connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            string updateQuery = "UPDATE Todos SET Task = @Task, IsCompleted = @IsCompleted WHERE Id = @Id;";
+            using (var command = new SQLiteCommand(updateQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Task", todo.Task);
+                command.Parameters.AddWithValue("@IsCompleted", todo.IsCompleted ? 1 : 0);
+                command.Parameters.AddWithValue("@Id", todo.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void DeleteTodo(int id)
+    {
+        using (var connection = new SQLiteConnection(connectionString))
+        {
+            connection.Open();
+            string deleteQuery = "DELETE FROM Todos WHERE Id = @Id;";
+            using (var command = new SQLiteCommand(deleteQuery, connection))
+            {
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
+
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Task { get; set; }
+    public bool IsCompleted { get; set; }
+public string TaskDisplay
+    {
+        get { return IsCompleted ? "[完了] " + Task : Task; }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstTodos = new System.Windows.Forms.ListBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(29, 28);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(238, 19);
+            this.txtTask.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(324, 28);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "追加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // lstTodos
+            // 
+            this.lstTodos.FormattingEnabled = true;
+            this.lstTodos.ItemHeight = 12;
+            this.lstTodos.Location = new System.Drawing.Point(29, 75);
+            this.lstTodos.Name = "lstTodos";
+            this.lstTodos.Size = new System.Drawing.Size(238, 268);
+            this.lstTodos.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(324, 75);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "更新";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(324, 124);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "削除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 415);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lstTodos);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTask);
+            this.Name = "Form1";
+            this.Text = "ToDoList";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstTodos;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
