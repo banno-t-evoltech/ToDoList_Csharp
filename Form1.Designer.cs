d9a1833e6519478ad578834c7b3f4dc5d9b8e710
sinneon <sinneon.world@gmail.com>
2025-06-09 14:08:22 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstTodos = new System.Windows.Forms.ListBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Location = new System.Drawing.Point(29, 33);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(238, 19);
+            this.txtTask.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(481, 55);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "追加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // lstTodos
+            // 
+            this.lstTodos.FormattingEnabled = true;
+            this.lstTodos.ItemHeight = 12;
+            this.lstTodos.Location = new System.Drawing.Point(29, 167);
+            this.lstTodos.Name = "lstTodos";
+            this.lstTodos.Size = new System.Drawing.Size(527, 184);
+            this.lstTodos.TabIndex = 2;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(481, 87);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "更新";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click_1);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(481, 116);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "削除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "追加したいタスクを上記に入力して追加ボタンを押してください。";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(424, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "変更したいタスクを選択し、上記に変更したいタスクを記述して更新ボタンを押してください。";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "削除したいタスクを選択して削除ボタンを押してください。";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "タスク一覧";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 415);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lstTodos);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTask);
+            this.Name = "Form1";
+            this.Text = "ToDoList";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstTodos;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        private DataAccess dataAccess;
+        private List<TodoItem> currentTodos;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataAccess = new DataAccess();
+            LoadTodos();
+        }
+
+        private void LoadTodos()
+        {
+            currentTodos = dataAccess.GetTodos();
+            lstTodos.DataSource = null;
+            lstTodos.DisplayMember = "TaskDisplay";
+            lstTodos.ValueMember = "Id";
+            lstTodos.DataSource = currentTodos;
+
+            // リストボックスの表示を更新
+            lstTodos.Refresh();
+        }
+
+        private void btnUpdate_Click_1(object sender, EventArgs e)
+        {
+            if (lstTodos.SelectedItem is TodoItem selectedTodo)
+            {
+                selectedTodo.Task = txtTask.Text.Trim();
+                dataAccess.UpdateTodo(selectedTodo);
+                txtTask.Clear();
+                LoadTodos();
+            }
+            else
+            {
+                MessageBox.Show("更新するタスクを選択してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                TodoItem newTodo = new TodoItem
+                {
+                    Task = txtTask.Text.Trim(),
+                    IsCompleted = false
+                };
+                dataAccess.AddTodo(newTodo);
+                txtTask.Clear();
+                LoadTodos();
+            }
+            else
+            {
+                MessageBox.Show("タスクを入力してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnDelete_Click_1(object sender, EventArgs e)
+        {
+            if (lstTodos.SelectedItem is TodoItem selectedTodo)
+            {
+                if (MessageBox.Show($"「{selectedTodo.Task}」を削除しますか？", "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    dataAccess.DeleteTodo(selectedTodo.Id);
+                    txtTask.Clear();
+                    LoadTodos();
+                }
+            }
+            else
+            {
+                MessageBox.Show("削除するタスクを選択してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /*
+        private void chkCompleted_CheckedChanged(object sender, EventArgs e)
+        {
+            if (lstTodos.SelectedItem is TodoItem selectedTodo)
+            {
+                txtTask.Text = selectedTodo.Task;
+            }
+            else
+            {
+                txtTask.Clear();
+            }
+        }
+        */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentTodos = dataAccess.GetTodos();
+            lstTodos.DataSource = null;
+            lstTodos.DisplayMember = "TaskDisplay";
+            lstTodos.ValueMember = "Id";
+            lstTodos.DataSource = currentTodos;
+
+            lstTodos.Refresh();
+        }
+    }
+}
